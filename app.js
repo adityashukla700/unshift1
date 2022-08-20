614a528c67b78df35bb58cd2c327bc43af8438a9
adityashukla700 <adityashukla7002@gmail.com>
2022-08-20 19:50:48 +0530

    allowing cross origin accesss2

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const app=express()
 const userRouter=require('./Routers/userRouter')
 const urlRouter=require('./Routers/urlRouter')
 const cors=require('cors')
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain)
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.json())
 app.use(cors());
 app.use('/api/user',userRouter)
