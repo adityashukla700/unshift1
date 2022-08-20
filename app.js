e5e37082fd03ed2a3a0b9fccf02c622e9b2f2e66
adityashukla700 <adityashukla7002@gmail.com>
2022-08-20 19:57:58 +0530

    allowing cross origin accesss3

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const urlRouter=require('./Routers/urlRouter')
 const cors=require('cors')
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials",true)
+    res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE,OPTIONS')
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
