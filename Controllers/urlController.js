8a9cdc3613048f4e93c350bba1bc36f98a8a7af5
adityashukla700 <adityashukla7002@gmail.com>
2022-08-20 19:45:35 +0530

    allowing cross origin accesss

--- Controllers/urlController.js
+++ Controllers/urlController.js
@@ -2,7 +2,12 @@ const {Url}=require('../Model/urlModel')
 const shortUrl = require("node-url-shortener");
 const valid_url=require("valid-url");
 const TinyURL=require('tinyurl')
- 
+const data={'url':'https://www.amazon.in/10000mAh-Lithium-Polymer-Warranty-CRSP10kPBA258901/dp/B09Z2XC3RG/?_encoding=UTF8&pd_rd_w=Um398&content-id=amzn1.sym.86bd9ba7-f177-459f-9995-c8e962dd9848&pf_rd_p=86bd9ba7-f177-459f-9995-c8e962dd9848&pf_rd_r=7M187SY7VESY8CC2DC0A&pd_rd_wg=2Phmf&pd_rd_r=0f1775d8-7983-4022-b97c-9a614bd5c48f&ref_=pd_gw_ci_mcx_mi','alias':'custom-alias-for-watch'}
+TinyURL.shortenWithAlias(data, function(resp, err) {
+    if (err)
+      console.log(err)
+    console.log(resp)
+});
 
 module.exports.saveUrl=async (req,res)=>{
     if(!valid_url.isHttpsUri(req.body.origUrl)){
@@ -19,13 +24,7 @@ module.exports.saveUrl=async (req,res)=>{
 }
 module.exports.customUrl=async(req,res)=>{
     // const data = { 'url':`${req.body.origUrl}`, 'alias': `custom-alias-for-${req.body.shortUrl}` }
-    // TinyURL.shortenWithAlias(data, function(resp, err) {
-    //     if (err)
-    //       console.log(err)
-    //       urlObject=Url({email:req.body.email,origUrl:req.body.origUrl,shortUrl:resp})
-    //       urlObject.save()
-    //       res.send(urlObject)
-    // });
+    
     res.send({message:"aditya shukla"});
 
 }
--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const app=express()
 const userRouter=require('./Routers/userRouter')
 const urlRouter=require('./Routers/urlRouter')
 const cors=require('cors')
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
+app.use(allowCrossDomain)
 app.use(express.json())
 app.use(cors());
 app.use('/api/user',userRouter)
